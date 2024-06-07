779776566593096d83dc7009ad61d54f9a99dd2b
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-07 17:27:03 +0300

    Initial backend project

---
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/DocspacePipedriveApplication.java
@@ -0,0 +1,12 @@
+package com.onlyoffice.docspacepipedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DocspacePipedriveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocspacePipedriveApplication.class, args);
+    }
+}
