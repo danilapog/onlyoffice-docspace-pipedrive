03c56d6a67c9814713d502b1750f197d0642d862
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-13 14:55:07 +0300

    feat(backend): delete DocspaceTokenService from SettingsService

--- backend/src/main/java/com/onlyoffice/docspacepipedrive/service/impl/SettingsServiceImpl.java
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/service/impl/SettingsServiceImpl.java
@@ -5,11 +5,9 @@ import com.onlyoffice.docspacepipedrive.entity.Settings;
 import com.onlyoffice.docspacepipedrive.exceptions.SettingsNotFoundException;
 import com.onlyoffice.docspacepipedrive.repository.SettingsRepository;
 import com.onlyoffice.docspacepipedrive.service.ClientService;
-import com.onlyoffice.docspacepipedrive.service.DocspaceTokenService;
 import com.onlyoffice.docspacepipedrive.service.SettingsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 
@@ -17,7 +15,6 @@ import org.springframework.util.StringUtils;
 @RequiredArgsConstructor
 public class SettingsServiceImpl implements SettingsService {
     private final ClientService clientService;
-    private final DocspaceTokenService docspaceTokenService;
     private final SettingsRepository settingsRepository;
 
     @Override
@@ -27,34 +24,22 @@ public class SettingsServiceImpl implements SettingsService {
     }
 
     @Override
-    @Transactional
     public Settings put(Long clientId, Settings settings) {
         Client client = clientService.findById(clientId);
 
         try {
             Settings existedSetting = findByClientId(clientId);
-            Boolean deleteToken = false;
 
-            if (StringUtils.hasText(settings.getUrl())
-                    && !existedSetting.getUrl().equals(settings.getUrl())) {
+            if (StringUtils.hasText(settings.getUrl())) {
                 existedSetting.setUrl(settings.getUrl());
-                deleteToken = true;
             }
 
-            if (StringUtils.hasText(settings.getUserName())
-                    && !existedSetting.getUserName().equals(settings.getUserName())) {
+            if (StringUtils.hasText(settings.getUserName())) {
                 existedSetting.setUserName(settings.getUserName());
-                deleteToken = true;
             }
 
-            if (StringUtils.hasText(settings.getPasswordHash())
-                    && !existedSetting.getPasswordHash().equals(settings.getPasswordHash())) {
+            if (StringUtils.hasText(settings.getPasswordHash())) {
                 existedSetting.setPasswordHash(settings.getPasswordHash());
-                deleteToken = true;
-            }
-
-            if (deleteToken) {
-                docspaceTokenService.deleteByClientId(clientId);
             }
 
             return settingsRepository.save(existedSetting);
