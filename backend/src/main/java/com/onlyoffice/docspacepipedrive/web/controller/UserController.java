7b9a59dc0506ef0cbdb6865255ae0a0fdbf6e2d9
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-13 16:05:43 +0300

    feat(backend): UserController (postDocspaceAccount, deleteDocspaceAccount)

--- backend/src/main/java/com/onlyoffice/docspacepipedrive/web/controller/UserController.java
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/web/controller/UserController.java
@@ -4,14 +4,20 @@ import com.onlyoffice.docspacepipedrive.client.docspace.DocspaceClient;
 import com.onlyoffice.docspacepipedrive.client.docspace.response.DocspaceUser;
 import com.onlyoffice.docspacepipedrive.client.pipedrive.PipedriveClient;
 import com.onlyoffice.docspacepipedrive.client.pipedrive.response.PipedriveUser;
+import com.onlyoffice.docspacepipedrive.entity.DocspaceAccount;
 import com.onlyoffice.docspacepipedrive.entity.User;
 import com.onlyoffice.docspacepipedrive.security.SecurityUtils;
+import com.onlyoffice.docspacepipedrive.service.DocspaceAccountService;
 import com.onlyoffice.docspacepipedrive.service.UserService;
+import com.onlyoffice.docspacepipedrive.web.dto.docspaceaccount.DocspaceAccountSaveRequest;
 import com.onlyoffice.docspacepipedrive.web.dto.user.UserResponse;
 import com.onlyoffice.docspacepipedrive.web.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final DocspaceAccountService docspaceAccountService;
     private final UserMapper userMapper;
     private final PipedriveClient pipedriveClient;
     private final DocspaceClient docspaceClient;
@@ -42,5 +49,29 @@ public class UserController {
         return ResponseEntity.ok(
                 userMapper.userToUserResponse(currentUser, pipedriveUser, docspaceUser)
         );
+    }
+
+    @PostMapping("/docspace-account")
+    public ResponseEntity<UserResponse> postDocspaceAccount(@RequestBody DocspaceAccountSaveRequest request) {
+        Long currentUserId = SecurityUtils.getCurrentUserId();
+
+        DocspaceUser docspaceUser = docspaceClient.getUser(request.getUserName());
+
+        DocspaceAccount docspaceAccount = new DocspaceAccount();
+        docspaceAccount.setId(docspaceUser.getId());
+        docspaceAccount.setPasswordHash(request.getPasswordHash());
+
+        docspaceAccountService.save(currentUserId, docspaceAccount);
+
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/docspace-account")
+    public ResponseEntity<Void> postDocspaceAccount() {
+        Long currentUserId = SecurityUtils.getCurrentUserId();
+
+        docspaceAccountService.deleteByUserId(currentUserId);
+
+        return ResponseEntity.noContent().build();
     }
 }
