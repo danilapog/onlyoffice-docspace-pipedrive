92165b2d326ebe3d74ff2d44c5385249310ffe18
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-18 09:04:48 +0300

    feat(frontend): added user service

---
+++ frontend/src/services/user.ts
@@ -0,0 +1,95 @@
+/**
+ *
+ * (c) Copyright Ascensio System SIA 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import axios from "axios";
+import axiosRetry from "axios-retry";
+import AppExtensionsSDK, { Command } from "@pipedrive/app-extensions-sdk";
+
+import { UserResponse } from "src/types/user";
+
+
+export const getUser = async (sdk: AppExtensionsSDK) => {
+  const pctx = await sdk.execute(Command.GET_SIGNED_TOKEN);
+  const client = axios.create({ baseURL: process.env.BACKEND_URL });
+  axiosRetry(client, {
+    retries: 2,
+    retryCondition: (error) => error.status !== 200,
+    retryDelay: (count) => count * 50,
+    shouldResetTimeout: true,
+  });
+
+  const response = await client<UserResponse>({
+    method: "GET",
+    url: `/api/v1/user`,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + pctx.token,
+    },
+    timeout: 15000,
+  });
+
+  return response.data;
+};
+
+export const postDocspaceAccount = async (
+  sdk: AppExtensionsSDK,
+  userName: string,
+  passwordHash: string
+) => {
+  const pctx = await sdk.execute(Command.GET_SIGNED_TOKEN);
+  const client = axios.create({ baseURL: process.env.BACKEND_URL });
+  axiosRetry(client, {
+    retries: 2,
+    retryCondition: (error) => error.status === 429,
+  });
+
+  await client({
+    method: "POST",
+    url: `/api/v1/user/docspace-account`,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + pctx.token,
+    },
+    data: {
+      userName: userName,
+      passwordHash: passwordHash,
+    },
+    timeout: 10000,
+  });
+};
+
+export const deleteDocspaceAccount = async (
+  sdk: AppExtensionsSDK
+) => {
+  const pctx = await sdk.execute(Command.GET_SIGNED_TOKEN);
+  const client = axios.create({ baseURL: process.env.BACKEND_URL });
+  axiosRetry(client, {
+    retries: 2,
+    retryCondition: (error) => error.status === 429,
+  });
+
+  await client({
+    method: "DELETE",
+    url: `/api/v1/user/docspace-account`,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + pctx.token,
+    },
+    timeout: 4000,
+  });
+};
