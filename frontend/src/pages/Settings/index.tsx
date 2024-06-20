3c31dc79e2e2a3a79f75f22108ec15a9c937160a
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-20 10:13:22 +0300

    feat(frontend): refactoring (Settings/index)

--- frontend/src/pages/Settings/index.tsx
+++ frontend/src/pages/Settings/index.tsx
@@ -18,80 +18,66 @@
 
 import React, { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { ConnectionSettings } from "./Connection";
-import { OnlyofficeBackgroundError } from "@layouts/ErrorBackground";
 
-import SettingsError from "@assets/settings-error.svg";
 import { AppContext } from "@context/AppContext";
+
 import { AuthorizationSetting } from "./Authorization";
+import { ConnectionSettings } from "./Connection";
+
+
+type Section = {
+  id: string,
+  title: string,
+  available: boolean,
+};
 
 export const SettingsPage: React.FC = () => {
   const { t } = useTranslation();
-  const { error } = useContext(AppContext);
+  const { user } = useContext(AppContext);
 
-  const [selectedSection, setSelectedSection] = useState<string>("conntection");
-
-  const sections = [
-    {id: "conntection",  title: t("settings.connection.menu-item", "Connection")},
-    {id: "authorization", title: t("settings.authorization.menu-item", "Authorization")},
+  const sections: Array<Section> = [
+    {id: "conntection",  title: t("settings.connection.menu-item", "Connection"), available: true},
+    {id: "authorization", title: t("settings.authorization.menu-item", "Authorization"), available: true},
   ];
 
-  console.log(error);
+  if (!(user?.is_admin && user.access.find((a) => a.app === "global" && a.admin))) {
+    sections[0].available = false;
+  }
+
+  const firstAvailableSection = sections.find((section) => section.available);
+
+  const [selectedSection, setSelectedSection] = useState<string|undefined>(firstAvailableSection?.id);
 
   return (
     <div className="w-screen h-screen">
-      {error && (
-        <OnlyofficeBackgroundError
-          Icon={<SettingsError />}
-          title={t("background.error.title", "Error")}
-          subtitle={t(
-            error.response?.status !== 401
-              ? "background.error.subtitle"
-              : "background.error.subtitle.token",
-            error.response?.status !== 401
-              ? "Could not fetch plugin settings. Something went wrong. Please reload the pipedrive window"
-              : "Could not fetch plugin settings. Something went wrong with your access token. Please reinstall the app"
-          )}
-          button={
-            error.response?.status === 401 && t("background.reinstall.button", "Reinstall") ||
-            "Reinstall"
-          }
-          onClick={error.response?.status === 401 ? () => {
-            if (error.response?.status === 401)
-              window.open(
-                // `${getCurrentURL().url}settings/marketplace`,
-                "_blank"
-              );
-          } : undefined}
-        />
-      )}
-      {!error && (
-        <div className="flex flex-row">
-          <div className="basis-1/5 border-r-2 p-1">
-            {sections.map(section => (
-              <div
-                id={section.id}
-                className={`text-left font border-spacing-7 px-10 py-2 m-1 rounded-lg cursor-pointer ${
-                  section.id === selectedSection
-                  ? "text-blue-600 font-medium bg-sky-100"
-                  : "hover:bg-stone-200"
-                }`}
-                onClick={() => setSelectedSection(section.id)}
-              >
-                {section.title}
-              </div>
-            ))}
-          </div>
-          <div className="basis-4/5 custom-scroll w-screen h-screen overflow-y-scroll overflow-x-hidden p-2">
-            {selectedSection === "conntection" &&
-              <ConnectionSettings />
-            }
-            {selectedSection === "authorization" &&
-              <AuthorizationSetting />
-            }
-          </div>
+      <div className="flex flex-row">
+        <div className="basis-1/5 border-r-2 p-1">
+          {sections.filter(section => section.available).map(section => (
+            <div
+              key={section.id}
+              id={section.id}
+              tabIndex={0}
+              className={`text-left font border-spacing-7 px-10 py-2 m-1 rounded-lg cursor-pointer ${
+                section.id === selectedSection
+                ? "text-blue-600 font-medium bg-sky-100"
+                : "hover:bg-stone-200"
+              }`}
+              onClick={() => setSelectedSection(section.id)}
+              onKeyDown={() => setSelectedSection(section.id)}
+            >
+              {section.title}
+            </div>
+          ))}
         </div>
-      )}
+        <div className="basis-4/5 custom-scroll w-screen h-screen overflow-y-scroll overflow-x-hidden p-2">
+          {selectedSection === "conntection" &&
+            <ConnectionSettings />
+          }
+          {selectedSection === "authorization" &&
+            <AuthorizationSetting />
+          }
+        </div>
+      </div>
     </div>
   );
 };
