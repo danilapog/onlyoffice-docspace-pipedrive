36de954c39dce5319d6e1d4ab09d411ec7802f55
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-26 11:14:42 +0300

    feat(frontend): appStatus service

---
+++ frontend/src/services/appStatus.ts
@@ -0,0 +1,45 @@
+/**
+ *
+ * (c) Copyright Ascensio System SIA 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import axios from "axios";
+import axiosRetry from "axios-retry";
+import AppExtensionsSDK, { Command } from "@pipedrive/app-extensions-sdk";
+import { AppStatusResponse } from "src/types/appStatus";
+
+export const getAppStatus = async (sdk: AppExtensionsSDK) => {
+  const pctx = await sdk.execute(Command.GET_SIGNED_TOKEN);
+  const client = axios.create({ baseURL: process.env.BACKEND_URL });
+  axiosRetry(client, {
+    retries: 2,
+    retryCondition: (error) => error.status !== 200,
+    retryDelay: (count) => count * 50,
+    shouldResetTimeout: true,
+  });
+
+  const response = await client<AppStatusResponse>({
+    method: "GET",
+    url: `/api/v1/app-status`,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + pctx.token,
+    },
+    timeout: 3000,
+  });
+
+  return response.data;
+};
