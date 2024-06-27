52a30be5c275fedca3952f3a68eadcc18e630c2c
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-27 13:48:53 +0300

    feat(backend): PipedriveDeal parameter visibleTo

--- backend/src/main/java/com/onlyoffice/docspacepipedrive/client/pipedrive/response/PipedriveDeal.java
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/client/pipedrive/response/PipedriveDeal.java
@@ -1,10 +1,14 @@
 package com.onlyoffice.docspacepipedrive.client.pipedrive.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 
 @Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PipedriveDeal {
     private Long id;
     private String title;
+    private Integer visibleTo;
 }
