890f5514d8c9ffee7c87bf3316bf12f6a848700f
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-07-16 16:43:51 +0300

    feat(backend): model DocspaceMembers

---
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/client/docspace/response/DocspaceMembers.java
@@ -0,0 +1,11 @@
+package com.onlyoffice.docspacepipedrive.client.docspace.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class DocspaceMembers {
+    private List<Object> members;
+}
