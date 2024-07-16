51c8572ead81d2ef9d459c40746e45793744d4be
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-07-16 16:30:41 +0300

    feat(backend): PipedriveResponse parameter additionalData

--- backend/src/main/java/com/onlyoffice/docspacepipedrive/client/pipedrive/response/PipedriveResponse.java
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/client/pipedrive/response/PipedriveResponse.java
@@ -1,10 +1,29 @@
 package com.onlyoffice.docspacepipedrive.client.pipedrive.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 
 @Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PipedriveResponse<T> {
     private Boolean success;
     private T data;
+    private PipedriveResponseAdditionalData additionalData;
+
+    @Data
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    public static class PipedriveResponseAdditionalData {
+        private PipedriveResponsePagination pagination;
+    }
+
+    @Data
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    public static class PipedriveResponsePagination {
+        private Integer start;
+        private Integer limit;
+        private Boolean moreItemsInCollection;
+        private Integer nextStart;
+    }
 }
