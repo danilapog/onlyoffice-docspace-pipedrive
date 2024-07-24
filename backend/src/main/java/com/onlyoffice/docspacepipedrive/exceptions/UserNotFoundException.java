ad210e5dd4ac5e41dab0dcf1c06d542091cee026
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-07-24 11:04:37 +0300

    fix(backend): UserNotFoundException message

--- backend/src/main/java/com/onlyoffice/docspacepipedrive/exceptions/UserNotFoundException.java
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/exceptions/UserNotFoundException.java
@@ -13,7 +13,7 @@ public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(Long userId, Long clientId) {
         super(MessageFormat.format(
-                "User with USER_ID ({0}) and CLIENT_ID ({0}) not found.",
+                "User with USER_ID ({0}) and CLIENT_ID ({1}) not found.",
                 userId.toString(),
                 clientId.toString()
         ));
