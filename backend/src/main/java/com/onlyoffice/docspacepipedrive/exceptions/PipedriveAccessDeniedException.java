5dd045b6e40e2bb78ec24581130d8ec5bfc2f000
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-09-12 12:32:15 +0300

    refactor(backend): PipedriveAccessDeniedException message

--- backend/src/main/java/com/onlyoffice/docspacepipedrive/exceptions/PipedriveAccessDeniedException.java
+++ backend/src/main/java/com/onlyoffice/docspacepipedrive/exceptions/PipedriveAccessDeniedException.java
@@ -25,6 +25,11 @@ import java.text.MessageFormat;
 
 public class PipedriveAccessDeniedException extends AccessDeniedException {
     public PipedriveAccessDeniedException(final Long userId) {
-        super(MessageFormat.format("Pipedrive user with ID ({0}) is not permitted to this operation.", userId));
+        super(
+                MessageFormat.format(
+                        "Pipedrive user with ID ({0}) is not permitted to this operation.",
+                        userId.toString()
+                )
+        );
     }
 }
